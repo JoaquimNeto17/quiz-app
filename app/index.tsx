448d49c0a63b68e796b717c27ea98afa1da1b079
JoaquimNeto17 <joaquim.neto.senai@gmail.com>
2026-08-11 14:09:36 -0300

    .

---
+++ app/index.tsx
@@ -0,0 +1,8 @@
+import QuizScreen from '../components/QuizScreen';
+import ExemploUseState from '../components/ExemploUseState';
+
+const HomePage = () => {
+  return <ExemploUseState />;
+};
+
+export default HomePage;
